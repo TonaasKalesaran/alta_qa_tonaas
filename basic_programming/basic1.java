4dedd056503e74f680d562bbe1b685f44b906fc1
Tonaas Kalesaran <tonaaskalesaranon@gmail.com>
2024-05-30 20:08:48 +0700

    ubah coding

--- basic_programming/basic1.java
+++ basic_programming/basic1.java
@@ -1,32 +1,12 @@
 package basic_programming;
 import java.util.Scanner;
 
-public class Main {
-
-    //function
-    private static boolean prima(int angka){
-        int total = 0;
-
-        for (int i = 1; i <= angka; i++) {
-            if (angka % i == 0) {
-                total += 1;
-            }
-        }
-
-        if (total == 2){
-            return true;
-        }else {
-            return false;
-        }
-    }
-
+public class basic1 {
     //main class
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("isi angka: ");
-        int isian = input.nextInt();
 
-        System.out.println(prima(isian));
     }
 }
