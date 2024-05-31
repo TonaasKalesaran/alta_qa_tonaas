415106705f86df9cea3b3e21a930c5af48f5c68a
Tonaas Kalesaran <tonaaskalesaranon@gmail.com>
2024-05-31 22:07:07 +0700

    add new basic3 inside ini_folder_baru

---
+++ ini_folder_baru/basic3.java
@@ -0,0 +1,18 @@
+package ini_folder_baru;
+
+public class basic3 {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
